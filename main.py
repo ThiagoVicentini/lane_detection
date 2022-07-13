b60dce43be6a64fcca34a65951e73c4e41bb0efa
murilo-toddy <murilo.todao@usp.br>
2022-07-13 11:01:06 -0300

    Update seeding values

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import imageio
 import contour
 import improc
 
-seed_path = ""
-image_test_path = "dataset_examples/um_000003.png"
+image_test_path = "dataset_examples/um_000052.png"
 masks_filename = "mask.json"
-debug = True
 generate_seed = False
 
 
@@ -19,9 +17,8 @@ if __name__ == "__main__":
 
     # Processes image to get lane edges
     processed_image = improc.detect_edges(image)
-    contour.find_contours(processed_image)
+    contours, cont_image = contour.find_contours(processed_image)
 
-    # Encontrar os contornos (filtro diferencial)
     # Pegar o centro dos contornos
     # Estimar a reta (interp linear?)
     # Coletar o ponto médio
