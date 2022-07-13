a23c91ba5365b642a85e0a9e99d3a77e3c85850e
murilo-toddy <murilo.todao@usp.br>
2022-07-13 11:00:09 -0300

    Implement contour detection function

--- contour.py
+++ contour.py
@@ -1,11 +1,25 @@
 import cv2
+import numpy as np
 
 
 def find_contours(image):
-    cv2.imshow("a", image)
-    cv2.waitKey(0)
-    contours, hierarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
+    """
+    Find contours in canny edged image
+    :param image: Input image
+    :return: Contours array and corresponding image
+    """
+    # Perform contour detection
+    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cont_image = np.zeros([image.shape[0], image.shape[1], 3])
 
-    cv2.imshow('Contours', image)
-    cv2.waitKey(0)
+    approximated_contours = []
+    # Approximate each contour by a simpler shape
+    for contour in contours:
+        epsilon = 0.01 * cv2.arcLength(contour, True)
+        # Approximate by a polygon and force it to be convex
+        approximated_contour = cv2.approxPolyDP(contour, epsilon, True)
+        convex_contour = cv2.convexHull(approximated_contour)
+        approximated_contours.append(convex_contour)
+        cv2.drawContours(cont_image, [convex_contour], -1, (0, 255, 0), 1)
+
+    return contours, cont_image
