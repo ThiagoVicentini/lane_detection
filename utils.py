c8f03c5a506e2f7d5b5043361a1afcd997b277b7
murilo-toddy <murilo.todao@usp.br>
2022-07-14 10:44:11 -0300

    File renaming

---
+++ utils.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+def show_image(image, name="Image"):
+    """
+    Shows an image
+    :param image: Image to show
+    :param name: Image name
+    :return: None
+    """
+    print(f"Showing {image}, press any key to close")
+    cv2.imshow(name, image)
+    cv2.waitKey()
