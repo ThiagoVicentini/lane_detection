5b684132e94901863a844ac37fb161f5e6abfcfa
murilo-toddy <murilo.todao@usp.br>
2022-07-17 23:59:19 -0300

    Update formatting

--- lane.py
+++ lane.py
@@ -1,18 +1,20 @@
-# Pegar o centro dos contornos
-# Estimar a reta (interp linear?)
-# Coletar o ponto médio
-
 import cv2
 import numpy as np
 from sklearn.cluster import KMeans
-from utils import show_image
+
 
 def find_lanes(contours, image):
+    """
+    Find lanes in a given set of contours
+    :param contours: List of contours
+    :param image: Original image
+    :return: Center road, New image
+    """
     new_image = np.zeros(image.shape)
     center_x_coord = int(image.shape[0]/2)
     center_y_coord = int(image.shape[1]/2)
 
-    #Compile all points in a single vector.
+    # Compile all points in a single vector.
     points = []
     for contour in contours:
         for vector in contour:
@@ -26,8 +28,8 @@ def find_lanes(contours, image):
     km = KMeans(n_clusters = 2, init='k-means++', n_init=200, max_iter=400, tol=1e-04)
     y = km.fit_predict(points[:, 1:2]).astype(np.int)
 
-    road1 = points[y==0]
-    road2 = points[y==1]
+    road1 = points[y == 0]
+    road2 = points[y == 1]
     
     for point in road1:
         cv2.circle(new_image, (int(point[0]), int(point[1])), radius=0, color=(0, 0, 255), thickness=-1)
@@ -37,7 +39,6 @@ def find_lanes(contours, image):
 
     road1_average = np.average(road1[:, 1])
     road2_average = np.average(road2[:, 1])
-    center_road = 0
     if abs(road1_average - center_y_coord) < abs(road2_average - center_y_coord):
         center_road = road1
     else:
@@ -53,7 +54,6 @@ def find_lane_middlepoint(center_road, image):
     """
     new_image = np.array(image)
     center_x_coord = int(image.shape[0]/2)
-    center_y_coord = int(image.shape[1]/2)
 
     max_negative_x = -np.inf
     max_negative_y = 0
